ac47f836b6b23fe5e51585f304e7fda71a4b3c67
Ellen Sandqvist <ell1sandqvist@gmail.com>
2025-07-04 12:13:23 +0200

    Restructured project to work with Vite

---
+++ main.js
@@ -0,0 +1,9 @@
+import "./js/background.js";
+import "./js/daily-wisdom.js";
+import "./js/date.js";
+import "./js/h1-header.js";
+import "./js/links.js";
+import "./js/notes.js";
+import "./js/weather.js";
+
+import "./css/main.css";
